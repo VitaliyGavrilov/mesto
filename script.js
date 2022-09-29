934d539f4646eced77acb07d88fba3b9e0be9273
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2022-09-29 14:30:06 +0300

    скоро на реввью

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-console.log('Привет мир!');
-//1) poup
+
+//1) poup close open
 
 //Получаем элементы
 const popupOpen = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.pop-up');
-const poupClose = document.querySelector('.close-button');
+const poupClose = document.querySelector('.pop-up__close-button');
 
 //создаем функцию
-const poupToggle = function(){
-    popup.classList.toggle('pop-up_opened');
+const poupToggle = function() {
+  popup.classList.toggle('pop-up_opened');
 };
 
 //Вешаем слушатель событий на редактирование профиля
@@ -17,4 +17,33 @@ popupOpen.addEventListener('click', poupToggle);
 //Вешаем слушатель событий на крестик 
 poupClose.addEventListener('click', poupToggle);
 
-//2) input
+//2) input save
+
+//Получаем элементы
+const popupSave = document.querySelector('.pop-up__save-button');
+
+const myNewName = document.querySelector('.pop-up__name');
+const myNewProfesion = document.querySelector('.pop-up__profesion');
+
+const myName = document.querySelector('.profile__name');
+const myProfesion = document.querySelector('.profile__profesion');
+
+//Создаем фунцию
+const editProfileSave = function () {
+  myName.textContent = myNewName.value;
+  myProfesion.textContent = myNewProfesion.value;
+}
+
+//Вешаем слушатель событий на save
+popupSave.addEventListener('click', editProfileSave);
+popupSave.addEventListener('click', poupToggle);
+
+// 3) unput-open
+
+// создаем фугкцию котрая переносит значения на поля при открытии попапа
+const editProfileOpen = function () {
+  myNewName.value = myName.textContent;
+  myNewProfesion.value = myProfesion.textContent;
+}
+// слушатель
+popupOpen.addEventListener('click', editProfileOpen);
