b6ea8f232725759d5d55f133d7af96769b7e7b60
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2022-10-09 09:03:19 +0300

    после ревью

--- script/script.js
+++ script/script.js
@@ -1,36 +1,38 @@
 //Получаем элементы для открытия попапа
-const popupOpen = document.querySelector('.profile__edit-button');
+const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const poupClose = document.querySelector('.popup__close-button');
+const popupCloseButton = document.querySelector('.popup__close-button');
 //Получаем элементы для сохранения
-const popupSave = document.querySelector('.popup__save-button');
-const myNewName = document.querySelector('.popup__name');
-const myNewProfesion = document.querySelector('.popup__profesion');
-const myName = document.querySelector('.profile__name');
-const myProfesion = document.querySelector('.profile__profesion');
+const popupSaveButton = document.querySelector('.popup__save-button');
+const popupInputName = document.querySelector('.popup__input_data_name');
+const popupInputProfession = document.querySelector('.popup__input_data_profesion');
+const popupForm = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profesion');
 //создаем функцию открытия попапа
-const poupToggle = function () {
+const popupToggle = function () {
   popup.classList.toggle('popup_opened');
 };
 //Создаем фунцию сохрания
-const editProfileSave = function () {
-  myName.textContent = myNewName.value;
-  myProfesion.textContent = myNewProfesion.value;
-}
+const formSubmitHandler = function (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupInputName.value;
+  profileProfession.textContent = popupInputProfession.value;
+  popupToggle();
+};
 // создаем фугкцию котрая переносит значения на поля при открытии попапа
-const editProfileOpen = function () {
-  myNewName.value = myName.textContent;
-  myNewProfesion.value = myProfesion.textContent;
-}
-//Вешаем слушатель событий на редактирование профиля
-popupOpen.addEventListener('click', poupToggle);
+const popupOpen = function () {
+  popupToggle();
+  popupInputName.value = profileName.textContent;
+  popupInputProfession.value = profileProfession.textContent;
+};
+// слушатель для открытия и переноса значении при открытии попапа
+profileEditButton.addEventListener('click', popupOpen);
 //Вешаем слушатель событий на крестик
-poupClose.addEventListener('click', poupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
 //Вешаем слушатель событий на save
-popupSave.addEventListener('click', editProfileSave);
-popupSave.addEventListener('click', poupToggle);
-// слушатель дляя переноса значении при открытии попапа
-popupOpen.addEventListener('click', editProfileOpen);
+popupForm.addEventListener('submit', formSubmitHandler);
+
 
 
 
